1d1b9b71d0b49304392020b94f65f231f36f85f8
Amy Berg <aberg2014@gmail.com>
2018-09-14 17:37:28 -0400

    components: Feedback highlights keywords

--- client/components/Feedback.js
+++ client/components/Feedback.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-const Feedback = ({keywords, variables, userText}) => {
+const Feedback = ({keywords, userText}) => {
   const words = userText.split(' ')
 
   function mapWordToSpan(word) {
     if (keywords[word]) {
       // it is a keyword, so we want to give it a span with some color
+      const color = keywords[word]
+      return <span style={{color}}>{word} </span>
     } else {
       return <span>{word} </span>
     }
@@ -15,14 +17,15 @@ const Feedback = ({keywords, variables, userText}) => {
   return (
     <div id="feedback">
       <h3>Your code:</h3>
-      <span>{userText}</span>
+      <span>{
+        words.map(word => mapWordToSpan(word))
+      }</span>
     </div>
   )
 }
 
 const mapState = state => ({
   keywords: state.keywords,
-  variables: state.variables,
   userText: state.userText
 })
 
