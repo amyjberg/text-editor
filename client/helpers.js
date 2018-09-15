7291a4f4b62dd0afad522209b55a0351d35545f5
Amy Berg <aberg2014@gmail.com>
2018-09-15 16:14:35 -0400

    helper functions: refactored to be DRY and tweaked getObjectSuggestions to suggest the right object keys

--- client/helpers.js
+++ client/helpers.js
@@ -8,22 +8,19 @@ const inOpenString = text => {
   return !!(allQuotes.length % 2 === 1)
 }
 
-const getObjectSuggestions = (variables, word) => {
-  const chainedText = word.split('.') // array of the properties we are chaining, e.g. ['obj', 'foo2']
-  let finalObjectInChain = variables[chainedText[0]]
+const getObjectSuggestions = (objects, word) => {
+  const chainedText = word.split('.')
+  let finalObjectInChain = objects[chainedText[0]]
   for (let i = 1; i < chainedText.length - 1; i++) {
-    // note: we don't want to count the last one because they might still be typing and the final property might not be finished
+    if (!finalObjectInChain) return [] // if we end up trying to access something that doesn't exist in memory, don't send suggestions
     finalObjectInChain = finalObjectInChain[chainedText[i]]
   }
 
   if (word[word.length - 1] === '.') { // we have all the options available
-    return Object.keys(finalObjectInChain)
-  } else if (word.indexOf('.') === -1) {
-    // they aren't trying to access properties, yet, so don't give them new suggestions
-    return []
+    // note: Object.keys of a string will return string indices, so we have to be sure to check here
+    return typeof finalObjectInChain === 'object' ? Object.keys(finalObjectInChain) : []
   } else {
     // they have started typing out a final property, so we want to give suggestions that match that property
-    // this should return the keys that are potential matches for what the user is typing
     const propertyUserIsTyping = chainedText[chainedText.length - 1]
     return Object.keys(finalObjectInChain).filter(key => {
       return key.indexOf(propertyUserIsTyping) !== -1
@@ -31,32 +28,42 @@ const getObjectSuggestions = (variables, word) => {
   }
 }
 
-export const getSuggestions = (keywords, variables, text) => {
-  if (inOpenString(text)) return [] // if we're in an open string, don't make suggestions
-
-  // for now, just base it off of the last word in the text
-  // limitation: if user moves cursor to middle of their text...
-  const words = text.split(' ')
-  const lastWord = words[words.length - 1] // maybe check here to see if there is a period, and if there is, check for the objects that have properties?
-  if (!lastWord) return [] // so we don't give suggestions if they haven't started typing out a new word yet
-
-  const keywordNames = Object.keys(keywords)
-  const variableNames = Object.keys(variables)
-  const allOptions = [...keywordNames, ...variableNames]
-  // how to deal with obj.foo2.foo3?
-  // convert objects in variables to the format obj.foo1, obj.foo2? then store them in an array?
-  // but we don't want all the suggestions to pop up right away -- only if they've typed obj do we see the rest of the suggestions
-
+const getMatches = (obj, word) => {
   const suggestions = []
-  for (let i = 0; i < allOptions.length; i++) {
-    if (allOptions[i].indexOf(lastWord) !== -1) {
-      suggestions.push(allOptions[i])
+  const names = Object.keys(obj)
+  for (let i = 0; i < names.length; i++) {
+    if (names[i].indexOf(word) !== -1) {
+      suggestions.push(names[i])
     }
   }
-
-
-
   return suggestions
+}
+
+const getLastWord = text => {
+  const words = text.split(' ')
+  return words[words.length - 1]
+}
+
+export const getSuggestions = (keywords, variables, text) => {
+  // for now, just base it off of the last word in the text
+  // limitation: if user moves cursor to middle of their text...
+  const lastWord = getLastWord(text)
+  if (inOpenString(text) || !lastWord) return [] // if we're in an open string or they haven't started the new word, don't make suggestions
+
+  // get all keyword matches
+  const keywordSuggestions = getMatches(keywords, lastWord)
+
+  // get all variable matches
+  let variableSuggestions
+  if (lastWord.indexOf('.') !== -1) {
+    // user is trying to access a proprety on an object
+    variableSuggestions = getObjectSuggestions(variables, lastWord)
+  } else {
+    // user is not trying to access a property
+    variableSuggestions = getMatches(variables, lastWord)
+  }
+
+  return [...keywordSuggestions, ...variableSuggestions]
 }
 
 // FEEDBACK HELPERS
